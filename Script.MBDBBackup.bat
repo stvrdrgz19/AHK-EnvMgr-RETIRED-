473a3eb1b47631bb77ccc1d516aa294e4614a623
stvrdrgz19 <stvrdrgz@gmail.com>
2019-04-03 07:48:39 -0400

    Saved changes to mbbackup script

---
+++ Script.MBDBBackup.bat
@@ -0,0 +1,31 @@
+@ECHO OFF 
+COLOR 0A
+cls
+
+SET SqlServer=%1
+SET Username=%2
+SET Password=%3
+SET LocalFolder=%4
+SET DataBases=%5
+SET DB1=%6
+SET DB2=%7
+SET DB3=%8
+
+if not exist "%LocalFolder%\%DataBases%" mkdir %LocalFolder%\%DataBases%
+
+echo.
+echo Backing up %DB1% to %LocalFolder%\%DataBases%. . .
+echo.
+SqlCmd -S %SqlServer% -U %Username% -P %Password% -Q "Backup Database %DB1% To Disk='%LocalFolder%\%DataBases%\%DB1%.bak' WITH INIT"
+
+echo.
+echo Backing up %DB2% to %LocalFolder%\%DataBases%. . .
+echo.
+SqlCmd -S %SqlServer% -U %Username% -P %Password% -Q "Backup Database %DB2% To Disk='%LocalFolder%\%DataBases%\%DB2%.bak' WITH INIT"
+
+echo.
+echo Backing up %DB3% to %LocalFolder%\%DataBases%. . .
+echo.
+SqlCmd -S %SqlServer% -U %Username% -P %Password% -Q "Backup Database %DB3% To Disk='%LocalFolder%\%DataBases%\%DB3%.bak' WITH INIT"
+
+REM TIMEOUT 5
